acd41a834269f5e1cfdfd4f695ed49c35866298a
electroms <mailinpro@gmail.com>
2023-04-12 15:36:46 +0200

    Create first script

---
+++ backup_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# On liste nos bases de données
+LISTEBDD=$( echo 'show databases' | mysql -u backup --password=< mot de passe >)
+for BDD in $LISTEBDD do
+# Exclusion des BDD information_schema , mysql et Database
+if [[ $BDD != "information_schema" ]] && [[ $BDD != "mysql" ]] && [[ $BDD != "Database" ]]; then
+# Emplacement du dossier ou nous allons stocker les bases de données, un dossier par base de données
+  CHEMIN=/home/user/save_BD/$BDD
+# On backup notre base de donnees
+  mysqldump -u backup --single-transaction --password= $BDD > "$CHEMIN/$BDD.sql"
+  echo "|Sauvegarde de la base de donnees $BDD.sql ";
+fi
+done
