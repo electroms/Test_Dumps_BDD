f0350772fa7ecab3c98e1fb07f908ff4194f93c1
electroms <mailinpro@gmail.com>
2023-04-13 09:23:01 +0200

    Script update

--- backup_script.sh
+++ backup_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # On liste nos bases de données
-LISTEBDD=$( echo 'show databases' | mysql -u backup --password=< mot de passe >)
+LISTEBDD=$( echo 'show databases' | mysql -u backup --password=< xWF8ux3g >)
 for BDD in $LISTEBDD do
 # Exclusion des BDD information_schema , mysql et Database
 if [[ $BDD != "information_schema" ]] && [[ $BDD != "mysql" ]] && [[ $BDD != "Database" ]]; then
 # Emplacement du dossier ou nous allons stocker les bases de données, un dossier par base de données
   CHEMIN=/home/user/save_BD/$BDD
 # On backup notre base de donnees
-  mysqldump -u backup --single-transaction --password= $BDD > "$CHEMIN/$BDD.sql"
-  echo "|Sauvegarde de la base de donnees $BDD.sql ";
+  mysqldump -u backup --single-transaction --password= $BDD > "$CHEMIN/OKOMAY.sql"
+  echo "|Sauvegarde de la base de donnees OKOMAY.sql ";
 fi
 done
